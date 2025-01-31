90399b49c37faed96d9a5d06a0dc8c7d22c4faee
valkyraycho <razor0424@gmail.com>
2025-01-30 19:48:43 -0800

    add user authorization

---
+++ api/auth.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"slices"
+	"strings"
+
+	"github.com/valkyraycho/bank_project/token"
+	"google.golang.org/grpc/metadata"
+)
+
+const (
+	authorizationHeader = "authorization"
+	authorizationBearer = "bearer"
+)
+
+func (s *Server) authorizeUser(ctx context.Context, accessibleRoles []string) (*token.Payload, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, fmt.Errorf("missing metadata")
+	}
+
+	auths := md.Get(authorizationHeader)
+	if len(auths) == 0 {
+		return nil, fmt.Errorf("missing authorization header")
+	}
+
+	fields := strings.Fields(auths[0])
+	if len(fields) < 2 {
+		return nil, fmt.Errorf("invalid authorization header")
+	}
+
+	authType := strings.ToLower(fields[0])
+	if authType != authorizationBearer {
+		return nil, fmt.Errorf("unsupported authorization type: %s", authType)
+	}
+
+	payload, err := s.tokenMaker.VerifyToken(fields[1])
+	if err != nil {
+		return nil, fmt.Errorf("invalid access token: %s", err)
+	}
+
+	if !slices.Contains(accessibleRoles, payload.Role) {
+		return nil, fmt.Errorf("permission denied")
+	}
+	return payload, err
+}
